66bba4d4a97e61581c6f43c2fb6101033779dd9e
Alexnader Hilberer <alexander.hilberer@gmail.com>
2026-09-15 07:27:09 +0200

    update

--- data/profile.js
+++ data/profile.js
@@ -3,7 +3,7 @@ window.APP = window.APP || { data: {}, i18n: {} };
 APP.data.profile = {
   name: "Alexander Hilberer",
   githubUrl: "https://github.com/ShollyWolly",
-  email: "alexander.hilberer@gmail.com",
+  email: "alexander@hilberer.dev",
   linkedinUrl: "https://www.linkedin.com/in/alexander-hilberer-166413194",
   resume: {
     de: "assets/resume/Alexander_Hilberer_Lebenslauf.pdf",
